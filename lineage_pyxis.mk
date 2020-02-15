95f9e2d5f98a49fbad3397f14f9637700a35379a
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-15 12:14:45 +0000

    pyxis-user 10 QKQ1.190828.002 20.1.21 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ lineage_pyxis.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from pyxis device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := pyxis
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_NAME := lineage_pyxis
+PRODUCT_MODEL := MI CC 9
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := pyxis
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="pyxis-user 10 QKQ1.190828.002 20.1.21 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/pyxis/pyxis:10/QKQ1.190828.002/20.1.21:user/release-keys
